ad3dada942696c3ec21993c20a9c9d87446f2688
ciotas <linzikristy@qq.com>
2021-12-12 16:27:03 +0800

    fix bug

--- resources/views/admin/tradingview.blade.php
+++ resources/views/admin/tradingview.blade.php
@@ -1,14 +1,14 @@
 <!-- TradingView Widget BEGIN -->
 <div class="tradingview-widget-container">
-  <div id="tradingview_427f2"></div>
-  <div class="tradingview-widget-copyright"><a href="https://cn.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL图表</span></a>由TradingView提供</div>
+  <div id="tradingview_adc76"></div>
+  <div class="tradingview-widget-copyright"><a href="https://cn.tradingview.com/symbols/ETHUSDTPERP/?exchange=BINANCE" rel="noopener" target="_blank"><span class="blue-text">ETHUSDTPERP图表</span></a>由TradingView提供</div>
   <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
   <script type="text/javascript">
   new TradingView.widget(
   {
   "width": 1080,
   "height": 610,
-  "symbol": "ETHUSDTPERP",
+  "symbol": "BINANCE:ETHUSDTPERP",
   "interval": "D",
   "timezone": "Etc/UTC",
   "theme": "dark",
@@ -18,10 +18,7 @@
   "enable_publishing": false,
   "hide_side_toolbar": false,
   "allow_symbol_change": true,
-  "show_popup_button": true,
-  "popup_width": "1000",
-  "popup_height": "650",
-  "container_id": "tradingview_427f2"
+  "container_id": "tradingview_adc76"
 }
   );
   </script>
